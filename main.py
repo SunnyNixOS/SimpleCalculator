f58eaf41c915cda546255863ee68c4ef9ec76ba9
Sunny <sun+nxisoftgithub@coinblock25.com>
2025-11-30 15:05:19 -0800

    first iteration - addition only

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-s = input('Name -->')
+number1 = input('Enter number: ')
+operation = input('Operation: ')
+number2 = input('Enter number: ')
 
-print(s)
+if operation == "+":
+    print('The solution is ' + str((int(number1)) + (int(number2))))
+else:
+    print('This program only supports addition dumdum')
